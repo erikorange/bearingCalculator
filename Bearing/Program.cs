f1b4498273ee8c8db566cb59c15c28b912fd0930
Erik Orange <erikorange@gmail.com>
2018-06-04 20:11:09 -0400

    Initial commit

---
+++ Bearing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bearing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double homeLat = 0;
+            double homeLon = 0;
+
+            double airLat = 0;
+            double airLon = 0;
+
+            double radFactor = (Math.PI / 180.0);
+
+            homeLat = homeLat * radFactor;
+            Console.WriteLine("homeLat: " + homeLat.ToString());
+
+            airLat = airLat * radFactor;
+            Console.WriteLine("airLat: " + airLat.ToString());
+
+            double diffLon = radFactor * (airLon - homeLon);
+            Console.WriteLine("diffLon: " + diffLon.ToString());
+
+            double x = Math.Sin(diffLon) * Math.Cos(airLat);
+            Console.WriteLine("x: " + x.ToString());
+
+            double y = Math.Cos(homeLat) * Math.Sin(airLat) - (Math.Sin(homeLat) * Math.Cos(airLat) * Math.Cos(diffLon));
+            Console.WriteLine("y: " + y.ToString());
+
+            double init_bearing = Math.Atan2(x, y);
+            Console.WriteLine("init_bearing: " + init_bearing.ToString());
+
+            init_bearing = init_bearing * (180.0 / Math.PI);
+            Console.WriteLine("init_bearing: " + init_bearing.ToString());
+
+            double compass_bearing = (init_bearing + 360.0) % 360.0;
+            Console.Write("Bearing: " + compass_bearing.ToString());
+            Console.ReadKey();
+        }
+    }
+}
